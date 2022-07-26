b1d740b2eadc67fedd5861b6bfaa678e9c682d91
makoto tanji <aiunwind@gmail.com>
2022-07-26 23:34:12 +0900

    Modify: gitignore

--- dogmeet-laravel-api/resources/views/welcome.blade.php
+++ dogmeet-laravel-api/resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-        <img src="storage/images/noimage.jpg" width="100" height="100">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -23,6 +22,7 @@
     </head>
     <body class="antialiased">
         <h1>{{ config('app.env', 'notfound') }}</h1>
+        <img src="storage/images/noimage.jpg" width="100" height="100">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
