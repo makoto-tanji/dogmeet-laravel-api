5265b4376fc16477bf16dd568978a721b3970b27
makoto tanji <aiunwind@gmail.com>
2022-07-26 10:31:10 +0900

    Modify: delete unused files

--- dogmeet-laravel-api/app/Http/Controllers/ColorController.php
+++ dogmeet-laravel-api/app/Http/Controllers/ColorController.php
@@ -20,49 +20,4 @@ class ColorController extends Controller
             'colorData' => $items
         ], 200);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Color  $color
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Color $color)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Color  $color
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Color $color)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Color  $color
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Color $color)
-    {
-        //
-    }
 }
--- dogmeet-laravel-api/app/Http/Controllers/ImageController.php
+++ dogmeet-laravel-api/app/Http/Controllers/ImageController.php
@@ -8,15 +8,6 @@ use Illuminate\Support\Facades\Hash;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -33,39 +24,5 @@ class ImageController extends Controller
         return response()->json([
             'imgName' => $name,
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
