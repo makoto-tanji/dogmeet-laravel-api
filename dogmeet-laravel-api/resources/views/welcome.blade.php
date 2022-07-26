af0b98a80cb9cdbb8ab0889340d3e8ec6f20beb4
makoto tanji <aiunwind@gmail.com>
2022-07-26 23:09:19 +0900

    Modify: link

--- dogmeet-laravel-api/resources/views/welcome.blade.php
+++ dogmeet-laravel-api/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-        <title>{{ config('app.env') }}</title>
+        <img src="storage/images/noimage.jpg" width="100" height="100">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
